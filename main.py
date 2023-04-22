6e49193136243cc93f823c33136693d1bea98356
gleb226 <gmedvede226@gmail.com>
2023-04-22 11:25:52 +0300

    added auto check erors and full conection to database

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
-import requests
-from bs4 import BeautifulSoup
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+try:
+    import requests
+    from bs4 import BeautifulSoup
+    from pymongo.mongo_client import MongoClient
+    from pymongo.server_api import ServerApi
+except Exception as e:
+    print(e)
 
 #help(requests)
 
-#URL1 = 'https://coinmarketcap.com/'
-URL2 = 'https://www.olx.ua/d/uk/transport/legkovye-avtomobili/'
-
-#r = requests.get(URL1)
-r = requests.get(URL2)
-
-data = r.text
-
-soup = BeautifulSoup (data, 'html.parser')
-
-c = soup.find_all('h6')
-
-# a = soup.find_all('a', {"href":"/currencies/bitcoin/markets/"
-# })
-# b = soup.find_all('a', {"href":"/currencies/ethereum/markets/"
-# })
-#
-# print(a[0].text)
-# print(b[0].text)
-
-for c in c:
-    print(c.text)
-
-uri = "mongodb+srv://gmedvede226:1234@cluster0.sx5570k.mongodb.net/?retryWrites=true&w=majority"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')conectet to database
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    #URL1 = 'https://coinmarketcap.com/'
+    URL2 = 'https://www.olx.ua/d/uk/transport/legkovye-avtomobili/'
+
+    #r = requests.get(URL1)
+    r = requests.get(URL2)
+
+    data = r.text
+
+    soup = BeautifulSoup (data, 'html.parser')
+
+    c = soup.find_all('h6')
+
+    # a = soup.find_all('a', {"href":"/currencies/bitcoin/markets/"
+    # })
+    # b = soup.find_all('a', {"href":"/currencies/ethereum/markets/"
+    # })
+    #
+    # print(a[0].text)
+    # print(b[0].text)
+
+    for c in c:
+        print(c.text)
 except Exception as e:
     print(e)
+
+try:
+    uri = "mongodb+srv://gmedvede226:1234@cluster0.sx5570k.mongodb.net/?retryWrites=true&w=majority"
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    # Send a ping to confirm a successful connection
+except Exception as e:
+    print(e)
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+
+    db = client['Medvediev']
+    collection = db['Items']
+    collection.insert_one({'Test':'test'})
+
+
+except Exception as e:
+    print(e)
+
