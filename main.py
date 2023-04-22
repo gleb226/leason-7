b78ca63cde69b0f4667ee809315a898b5030395a
gleb226 <gmedvede226@gmail.com>
2023-04-22 10:52:18 +0300

    added models cars from olx

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ from bs4 import BeautifulSoup
 
 #help(requests)
 
-URL = 'https://coinmarketcap.com/'
+#URL1 = 'https://coinmarketcap.com/'
+URL2 = 'https://www.olx.ua/d/uk/transport/legkovye-avtomobili/'
 
-r = requests.get(URL)
+#r = requests.get(URL1)
+r = requests.get(URL2)
 
 data = r.text
 
 soup = BeautifulSoup (data, 'html.parser')
 
-a = soup.find_all('a', {"href":"/currencies/bitcoin/markets/"
-})
-b = soup.find_all('a', {"href":"/currencies/ethereum/markets/"
-})
+c = soup.find_all('h6')
 
-print(a[0].text)
-print(b[0].text)
+# a = soup.find_all('a', {"href":"/currencies/bitcoin/markets/"
+# })
+# b = soup.find_all('a', {"href":"/currencies/ethereum/markets/"
+# })
+#
+# print(a[0].text)
+# print(b[0].text)
+
+for c in c:
+    print(c.text)
